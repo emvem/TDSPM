5482b8fcbef8129c42be0d87f723c3a8c720d381
User <user@MacBook-Pro-User.local>
2023-02-06 15:00:40 +0600

    spm dependencies specified

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "TDSPM",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TDSPM",
             targets: ["TDSPM"]),
